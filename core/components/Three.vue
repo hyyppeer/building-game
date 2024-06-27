3e32ee8016b2ffa097a21c01be9ec80cb196ff5b
hyper <95050519+hyyppeer@users.noreply.github.com>
2024-06-27 12:39:44 +0300

    refactoring

--- core/components/Three.vue
+++ core/components/Three.vue
@@ -5,38 +5,40 @@ import { generateTerrain } from '../terrainGeneration'
 </script>
 
 <script>
-const scene = new THREE.Scene()
-const camera = new THREE.PerspectiveCamera(
-  75,
-  window.innerWidth / window.innerHeight,
-  0.1,
-  50
-)
+function init() {
+  const scene = new THREE.Scene()
+  const camera = new THREE.PerspectiveCamera(
+    75,
+    window.innerWidth / window.innerHeight,
+    0.1,
+    500
+  )
 
-const renderer = new THREE.WebGLRenderer()
-renderer.setSize(window.innerWidth, window.innerHeight)
-renderer.setAnimationLoop(animate)
+  const renderer = new THREE.WebGLRenderer()
+  renderer.setSize(window.innerWidth, window.innerHeight)
+  renderer.setAnimationLoop(animate)
 
-const clock = new THREE.Clock()
-clock.start()
+  document.body.appendChild(renderer.domElement)
 
-const controls = new FlyControls(camera, renderer.domElement)
-controls.movementSpeed = 1
-controls.rollSpeed = 0.5
+  const controls = new FlyControls(camera, renderer.domElement)
+  controls.movementSpeed = 1
+  controls.rollSpeed = 0.05
 
-document.body.appendChild(renderer.domElement)
+  const clock = new THREE.Clock()
+  clock.start()
 
-scene.add(new THREE.AmbientLight(0xffffff, 5))
+  scene.add(new THREE.AmbientLight(0xffffff, 5))
 
-camera.position.y = 3
+  camera.position.y = 3
 
-generateTerrain().forEach((tri) => {
-  const geo = new THREE.BufferGeometry()
-  geo.setFromPoints(tri)
-  const mat = new THREE.MeshStandardMaterial({ color: 0x55ff55 })
-  const mesh = new THREE.Mesh(geo, mat)
-  scene.add(mesh)
-})
+  generateTerrain().forEach((tri) => {
+    const geo = new THREE.BufferGeometry()
+    geo.setFromPoints(tri)
+    const mat = new THREE.MeshStandardMaterial({ color: 0x55ff55 })
+    const mesh = new THREE.Mesh(geo, mat)
+    scene.add(mesh)
+  })
+}
 
 function animate() {
   controls.update(clock.getDelta())
@@ -46,6 +48,6 @@ function animate() {
 window.addEventListener('resize', () => {
   renderer.setSize(window.innerWidth, window.innerHeight)
 })
-</script>
 
-<template></template>
+init()
+</script>
--- core/game.ts
+++ core/game.ts
@@ -1,4 +1,4 @@
 import { createApp } from 'vue'
-import App from './App.vue'
+import Game from './Game.vue'
 
-createApp(App).mount('#app')
+createApp(Game).mount('#app')
